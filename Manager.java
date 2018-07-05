a87f2dba8551f6881c8494d842503fdb2a013984
Ali-Abdolkarim <37148499+Ali-Abdolkarim@users.noreply.github.com>
2018-07-05 16:17:32 +0300

    updated the file

--- Manager.java
+++ Manager.java
@@ -5,8 +5,12 @@ import android.util.Log;
 public class Manager extends employe{
     public void countSalary ()
     {
-        Log.i("manager", "countSalary:4000");
-    }
+        Log.i("manager", "countSalary:4000");// so i have to write something here 
+    }// as well as here , never mind 
+    //writting comment is a good thing 
+    //comments would make code easier to read
+    // code is a good why of describing the things 
+    // really  i don't know what i am talking about
     //override is the same method that we use in the employe but with bifferent code or jop
     //child class cuz of extends
 }
